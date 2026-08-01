f138ea98e6cca629b228195f8a06bbd8344caa6d
esapermana19 <esapermanami24@gmail.com>
2026-08-01 19:52:21 +0700

    Fix signature_url to use relative path /storage/ for correct port/domain image loading

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,7 +55,7 @@ class User extends Authenticatable
             return $this->signature;
         }
 
-        return asset('storage/' . ltrim($this->signature, '/'));
+        return '/storage/' . ltrim($this->signature, '/');
     }
 
     /**
